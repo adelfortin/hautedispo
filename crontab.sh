0da01b9b224a5eeaac4fe54b0825a71b4e244e51
Adel Fortin <120451873+adelfortin@users.noreply.github.com>
2023-04-12 08:44:31 +0200

    Update crontab.sh

--- crontab.sh
+++ crontab.sh
@@ -3,11 +3,12 @@
 # set variables
 LOCAL_DIR=/path/to/local/directory
 REMOTE_USER=username
-REMOTE_HOST=example.com
+REMOTE_HOST_A=example.com
+REMOTE_HOST_B=example.com
 REMOTE_DIR=/path/to/remote/directory
 
 # synchronize local and remote directories using rsync and ssh
-rsync -avz --delete --exclude='.git/' -e ssh $LOCAL_DIR/ $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/
+ssh $REMOTE_USER@$REMOTE_HOST_A "ssh $REMOTE_USER@$REMOTE_HOST_B 'cd $REMOTE_DIR; /path/to/your/sync.sh'"
 
 # output log message
 echo "Directory synchronized at $(date)"
