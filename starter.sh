2432a7d7524a764f4a27cf5eb61a524d5f3fcf46
Adel Fortin <120451873+adelfortin@users.noreply.github.com>
2023-04-14 11:29:55 +0200

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,1 +1,51 @@
 
+#!/bin/bash
+
+# Variables liées à l'utilisateur et au déploiement
+UTILISATEUR=$(whoami)
+REPO_GIT="https://github.com/adelfortin/hautedispo"
+
+# Variables liées aux chemins
+REPERTOIRE_CIBLE="/home/$UTILISATEUR/.deploy"
+CHEMIN_INVENTAIRE="$REPERTOIRE_CIBLE/inventaire.ini"
+CHEMIN_DEPLOIEMENT="$REPERTOIRE_CIBLE/deploiement.yml"
+
+# Variables liées à Ansible
+UTILISATEUR_ANSIBLE="root"
+MOT_DE_PASSE_ANSIBLE="master"
+
+# Installation de Git et Ansible
+dnf install -y git ansible
+
+# Création du répertoire de déploiement
+mkdir -p "$REPERTOIRE_CIBLE"
+
+# Clone du dépôt Git
+if git clone "$REPO_GIT" "$REPERTOIRE_CIBLE"; then
+echo "Le dépôt a été cloné avec succès."
+else
+echo "Une erreur est survenue lors du clonage du dépôt."
+exit 1
+fi
+
+# Vérification et modification des fichiers de configuration Ansible
+while IFS= read -r ligne; do
+if [[ "$ligne" =~ ^\s*# ]] || [[ -z "$ligne" ]]; then
+continue
+fi
+sed -i "/$ligne/d" /etc/ansible/hosts
+done < /etc/ansible/hosts
+
+if ! grep -q "remote_user=root" /etc/ansible/ansible.cfg; then
+echo "[defaults]" >> /etc/ansible/ansible.cfg
+echo "remote_user=$UTILISATEUR_ANSIBLE" >> /etc/ansible/ansible.cfg
+echo "remote_pass=$MOT_DE_PASSE_ANSIBLE" >> /etc/ansible/ansible.cfg
+echo "Le fichier /etc/ansible/ansible.cfg a été modifié avec succès."
+else
+echo "Le fichier /etc/ansible/ansible.cfg ne doit pas être modifié."
+fi
+
+# Exécution de la commande Ansible
+COMMANDE_ANSIBLE="ansible-playbook -i $CHEMIN_INVENTAIRE $CHEMIN_DEPLOIEMENT"
+echo "Exécution de la commande Ansible : $COMMANDE_ANSIBLE"
+$COMMANDE_ANSIBLE
