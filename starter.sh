98025e15ae87406c7164511e48a5df1d5e58192f
Rudy <46909788+SHaddow7@users.noreply.github.com>
2023-04-17 12:06:35 +0200

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Variables liées à l'utilisateur et au déploiement
@@ -31,27 +30,28 @@ mkdir -p "$REPERTOIRE_CIBLE"
 
 # Clone du dépôt Git
 if git clone "$REPO_GIT" "$REPERTOIRE_CIBLE"; then
-echo "Le dépôt a été cloné avec succès."
+    echo "Le dépôt a été cloné avec succès."
 else
-echo "Une erreur est survenue lors du clonage du dépôt."
-exit 1
+    echo "Une erreur est survenue lors du clonage du dépôt."
+    exit 1
 fi
 
 # Vérification et modification des fichiers de configuration Ansible
 while IFS= read -r ligne; do
-if [[ "$ligne" =~ ^\s*# ]] || [[ -z "$ligne" ]]; then
-continue
-fi
-sed -i "/$ligne/d" /etc/ansible/hosts
-done < /etc/ansible/hosts
+    if [[ "$ligne" =~ ^\s*# ]] || [[ -z "$ligne" ]]; then
+        continue
+    fi
+    sed -i "/$ligne/d" /etc/ansible/hosts
+done </etc/ansible/hosts
 
+# Configuration du fichier de cfg de ANSIBLE
 if ! grep -q "remote_user=root" /etc/ansible/ansible.cfg; then
-echo "[defaults]" >> /etc/ansible/ansible.cfg
-echo "remote_user=$UTILISATEUR_ANSIBLE" >> /etc/ansible/ansible.cfg
-echo "remote_pass=$MOT_DE_PASSE_ANSIBLE" >> /etc/ansible/ansible.cfg
-echo "Le fichier /etc/ansible/ansible.cfg a été modifié avec succès."
+    echo "[defaults]" >>/etc/ansible/ansible.cfg
+    echo "remote_user=$UTILISATEUR_ANSIBLE" >>/etc/ansible/ansible.cfg
+    echo "remote_pass=$MOT_DE_PASSE_ANSIBLE" >>/etc/ansible/ansible.cfg
+    echo "Le fichier /etc/ansible/ansible.cfg a été modifié avec succès."
 else
-echo "Le fichier /etc/ansible/ansible.cfg ne doit pas être modifié."
+    echo "Le fichier /etc/ansible/ansible.cfg ne doit pas être modifié."
 fi
 
 # Exécution de la commande Ansible
