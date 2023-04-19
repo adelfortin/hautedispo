d5e576b80cc76a7e2d65f60c11cffbd8523655b8
Adel Fortin <120451873+adelfortin@users.noreply.github.com>
2023-04-19 08:00:16 +0200

    Update cloture8.sh

--- cloture8.sh
+++ cloture8.sh
@@ -59,6 +59,46 @@ usage
 ;;
 esac
 
+function supprimer_connexion() {
+# Obtenir l'UUID de la connexion associée à l'interface voulu
+uuid=$(nmcli -g UUID con show "$1")
+
+# Vérifier si l'UUID a été trouvé
+if [ -z "$uuid" ]
+then
+# Si l'UUID n'a pas été trouvé, afficher un message d'erreur
+echo "La connexion pour l'interface $1 n'a pas été trouvée."
+return 1
+else
+# Si l'UUID a été trouvé, afficher un message de confirmation
+echo "La connexion pour l'interface $1 a été trouvée : $uuid."
+fi
+
+# Supprimer la connexion associée à l'interface voulu
+nmcli con delete "$uuid"
+
+# Vérifier si la suppression a réussi
+if [ $? -eq 0 ]
+then
+# Si la suppression a réussi, afficher un message de confirmation
+echo "La connexion pour l'interface enp0s3 a été supprimée."
+else
+# Sinon, afficher un message d'erreur
+echo "La suppression de la connexion pour l'interface enp0s3 a échoué."
+return 1
+fi
+}
+# Vérifie les privilèges d'administrateur
+if [[ $(id -u) -ne 0 ]]; then
+echo "Erreur : cette action nécessite des privilèges d'administrateur."
+exit 1
+fi
+
+# Supprimer les connexions non nécessaire
+echo "Supprimer les connexions non nécessaire"
+supprimer_connexion enp0s3
+supprimer_connexion enp0s8
+
 # Redémarrage de la machine
 echo "Redémarrage de la machine..."
 reboot
