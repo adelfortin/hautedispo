852b305c53835db4a888a5ce31041555150e02b4
Adel Fortin <120451873+adelfortin@users.noreply.github.com>
2023-04-20 09:13:45 +0200

    Update scan_ip.sh

--- scan_ip.sh
+++ scan_ip.sh
@@ -32,7 +32,7 @@ function find_adresses {
 
 function add_adresses {
     # Ajout de l'adresse de loopback dans le fichier des adresses
-    echo "127.0.0.1" >>"${fichier_ip}"
+    #echo "127.0.0.1" >>"${fichier_ip}"
 }
 
 find_adresses
