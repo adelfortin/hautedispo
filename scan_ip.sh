f01921d1394a1ed10c440a1d5667add313f5f670
Rudy <46909788+SHaddow7@users.noreply.github.com>
2023-04-20 08:19:55 +0200

    Update scan_ip.sh

--- scan_ip.sh
+++ scan_ip.sh
@@ -1,99 +1,42 @@
 #!/bin/bash
+# ---------------------#
+# Desc.   : Ce script permet de garder en cache une liste d'adresse ip des machines non configuré
+# Date    :
+# Version :
+# ---------------------#
 
-# Définir le nom de l'interface à scanner
-INTERFACE="enp0s3"
+prefix_adresse="192.168.58"
+adresses_bannies=("192.168.58.1" "192.168.58.2")
+nombre_adresse_voulue=3
+interface="enp0s3"
+ip_actuel=$(ip addr show dev $interface | grep "inet " | awk '{print $2}' | cut -d/ -f1) # Adresse ip de la machine actuel
+fichier_decouverte="/root/.deploy/adresses_decouverte.log"
+fichier_ip="/root/.deploy/adresse_ipv4.log"
 
-# Obtenir l'adresse IP de l'interface
-ip=$(ip addr show dev $INTERFACE | grep "inet " | awk '{print $2}' | cut -d/ -f1)
-
-# Stocker l'adresse IP dans une variable liée
-IP_MACHINE=$ip
-
-# Définir les adresses bannies
-ADRESSES_BANNIES=("192.168.58.1" "192.168.58.2")
-
-# Fonction pour obtenir le nombre d'adresses IPv4 dans le même réseau que la machine
-function nombre_ipv4() {
-# Extraire le préfixe du réseau à partir de l'adresse IP
-prefix=$(echo $ip | cut -d. -f1-3)
-
-# Obtenir la liste de toutes les adresses IPv4 dans le même réseau que la machine
-adresses=$(nmap -sL $prefix.0/24 | grep "Nmap scan report for" | awk '{print $NF}')
-
-# Compter le nombre d'adresses IPv4 dans la liste
-nombre=$(echo "$adresses" | wc -l)
-
-# Soustraire 1 pour obtenir le nombre d'adresses IPv4 autres que celle de la machine
-nombre=$(expr $nombre - 1)
-
-#
-nombre_personalise=8
-
-# Stocker le nombre dans une variable liée
-NOMBRE_IP=$nombre
+# Cette fonction permet de liste tous les adresses IP qui sont vivant
+# sur le réseau
+function find_adresses {
+    # Récupere uniquement les adresses qui sont alive
+    fping -aqg ${prefix_adresse}.0/24 >$fichier_decouverte
+    # Trie en fonction des adresses bannies
+    while IFS= read -r adresse; do
+        if [ "$adresse" != "$ip_actuel" ] && [[ ! ${adresses_bannies[*]} =~ ${adresse} ]]; then
+            echo "$adresse" >${fichier_ip}
+        fi
+    done <"$fichier_decouverte"
+    # Récupere au moin le nombre d'adresse suffisant
+    head -n ${nombre_adresse_voulue} ${fichier_ip} >tmp && mv tmp ${fichier_ip}
+    # Suppression du fichier des adresses découverte
+    rm -f ${fichier_decouverte}
 }
 
-# Appeler la fonction pour obtenir le nombre d'adresses IPv4
-nombre_ipv4
-
-# Fonction pour scanner toutes les adresses IPv4 dans le même réseau que la machine
-function scan_adresses() {
-# Définir le nom du fichier de sortie
-nom_fichier="/root/.deploy/adresse_ipv4.log"
-
-# Vérifier si le fichier existe déjà
-if [ -e $nom_fichier ]
-then
-# Si le fichier existe, ajouter les résultats à la fin
-mode="a"
-else
-# Sinon, créer un nouveau fichier
-mode="w"
-fi
-
-# Ouvrir le fichier en mode écriture ou ajout
-exec 3<> $nom_fichier
-
-# Initialiser la variable pour compter les adresses répondant au ping
-i=0
-
-# Boucle pour scanner toutes les adresses IPv4
-for adresse in $adresses
-do
-# Vérifier si l'adresse est celle de la machine ou si elle est bannie
-if [ "$adresse" != "$ip" ] && [[ ! " ${ADRESSES_BANNIES[@]} " =~ " ${adresse} " ]]
-then
-# Si l'adresse n'est pas celle de la machine ni bannie, effectuer un ping
-if ping -c 1 -w 1 $adresse > /dev/null
-then
-# Si la réponse est positive, incrémenter le compteur et écrire dans le fichier
-i=$(expr $i + 1)
-echo "$adresse" >&3
-echo "Adresse $adresse a répondu au ping." >> log.txt
-else
-# Sinon, ne rien faire
-echo "Adresse $adresse n'a pas répondu au ping." >> log.txt
-fi
-fi
-
-# Vérifier si le nombre maximum d'adresses a été atteint
-if [ $i -ge $NOMBRE_IP ]
-then
-# Si le nombre maximum a été atteint, sortir de la boucle
-echo "Le script a essayé tout les adresses possibles"
-break
-elif [ $i -ge $nombre_personalise ]
-then
-echo "Le script a trouvé le nombre d'adresse attendu sur ce réseau."
-break
-fi
-done
-echo "127.0.0.1" >&3
-
+function add_adresses {
+    # Ajout de l'adresse de loopback dans le fichier des adresses
+    echo "127.0.0.1" >>"${fichier_ip}"
 }
-usage() {
-  nombre_ipv4
-  scan_adresses
-}
-usage  
-# Fermer
+
+find_adresses
+add_adresses
+
+# Fin du programme
+exit 0
