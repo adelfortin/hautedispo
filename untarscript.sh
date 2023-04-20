42b2012f46af7f220c560daf9c31dfa0c0f2c1e1
Adel Fortin <120451873+adelfortin@users.noreply.github.com>
2023-04-20 18:25:25 +0200

    Update untarscript.sh

--- untarscript.sh
+++ untarscript.sh
@@ -35,14 +35,39 @@ function extraire_archive {
     fi
 }
 
+# Fonction pour exécuter un script en mode sudo
+# Prend un paramètre : le chemin du script à exécuter
+executer_script_sudo() {
+script="$1"
+
+# Vérifier si le fichier de script existe
+if [ -f "$script" ]; then
+# Exécuter le script avec sudo
+sudo bash "$script"
+
+# Vérifier si l'exécution a réussi
+if [ $? -eq 0 ]; then
+echo "Le script '$script' a été exécuté avec succès en mode sudo."
+else
+echo "Erreur lors de l'exécution du script '$script' en mode sudo."
+return 1
+fi
+else
+echo "Le fichier '$script' n'existe pas ou n'est pas un fichier de script."
+return 1
+fi
+
+return 0
+}
+
 function usage {
     creer_dossier "$DOSSIER_DESTINATION"
     extraire_archive
+    executer_script_sudo "$DOSSIER_DESTINATION/install.sh"
 }
 
 # Programme principal
 usage
-bash "$DOSSIER_CLONE/install.sh"
 
 # Fin du programme
 exit 0
