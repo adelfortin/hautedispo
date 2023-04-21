e88acb606bdbb331c3730973d46fe681de5021f4
Rudy <46909788+SHaddow7@users.noreply.github.com>
2023-04-21 14:56:02 +0200

    Update cloturemachine1.sh

--- cloturemachine1.sh
+++ cloturemachine1.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 
-function supprimer_connexion {
-  # Obtenir l'UUID de la connexion associée à l'interface enp0s3
-  uuid=$(nmcli -g UUID con show "enp0s3")
+function supprimer_connexion() {
+    # Obtenir l'UUID de la connexion associée à l'interface enp0s3
+    uuid=$(nmcli -g UUID con show "enp0s3")
 
-  # Vérifier si l'UUID a été trouvé
-  if [ -z "$uuid" ]; then
-    # Si l'UUID n'a pas été trouvé, afficher un message d'erreur
-    echo "La connexion pour l'interface enp0s3 n'a pas été trouvée."
-    return 1
-  else
-    # Si l'UUID a été trouvé, afficher un message de confirmation
-    echo "La connexion pour l'interface enp0s3 a été trouvée : $uuid."
-  fi
+    # Vérifier si l'UUID a été trouvé
+    if [ -z "$uuid" ]; then
+        # Si l'UUID n'a pas été trouvé, afficher un message d'erreur
+        echo "La connexion pour l'interface enp0s3 n'a pas été trouvée."
+        return 1
+    else
+        # Si l'UUID a été trouvé, afficher un message de confirmation
+        echo "La connexion pour l'interface enp0s3 a été trouvée : $uuid."
+    fi
 
-  # Supprimer la connexion associée à l'interface enp0s3
-  nmcli con delete "$uuid"
-
-  # Vérifier si la suppression a réussi
-  if [ $? -eq 0 ]; then
-    # Si la suppression a réussi, afficher un message de confirmation
-    echo "La connexion pour l'interface enp0s3 a été supprimée."
-  else
-    # Sinon, afficher un message d'erreur
-    echo "La suppression de la connexion pour l'interface enp0s3 a échoué."
-    return 1
-  fi
+    # Supprimer la connexion associée à l'interface enp0s3
+    # Vérifier si la suppression a réussi
+    if nmcli con delete "$uuid"; then
+        # Si la suppression a réussi, afficher un message de confirmation
+        echo "La connexion pour l'interface enp0s3 a été supprimée."
+    else
+        # Sinon, afficher un message d'erreur
+        echo "La suppression de la connexion pour l'interface enp0s3 a échoué."
+        return 1
+    fi
 }
 # Vérifie les privilèges d'administrateur
 if [[ $(id -u) -ne 0 ]]; then
-  echo "Erreur : cette action nécessite des privilèges d'administrateur."
-  exit 1
+    echo "Erreur : cette action nécessite des privilèges d'administrateur."
+    exit 1
 fi
 
 # Supprimer les connexions non nécessaire
@@ -38,30 +36,26 @@ echo "Supprimer les connexions non nécessaire"
 supprimer_connexion
 
 # Fonction pour redémarrer le service de gestionnaire de réseau
-function redemarrer_service_reseau {
-  # Arrêter le service de gestionnaire de réseau
-  systemctl stop NetworkManager
+function redemarrer_service_reseau() {
+    # Arrêter le service de gestionnaire de réseau
+    # Vérifier si l'arrêt du service a réussi
+    if systemctl stop NetworkManager; then
+        echo "Le service de gestionnaire de réseau a été arrêté avec succès."
+    else
+        echo "Erreur lors de l'arrêt du service de gestionnaire de réseau."
+        return 1
+    fi
 
-  # Vérifier si l'arrêt du service a réussi
-  if [ $? -eq 0 ]; then
-    echo "Le service de gestionnaire de réseau a été arrêté avec succès."
-  else
-    echo "Erreur lors de l'arrêt du service de gestionnaire de réseau."
-    return 1
-  fi
+    # Redémarrer le service de gestionnaire de réseau
+    # Vérifier si le redémarrage du service a réussi
+    if systemctl restart NetworkManager; then
+        echo "Le service de gestionnaire de réseau a été redémarré avec succès."
+    else
+        echo "Erreur lors du redémarrage du service de gestionnaire de réseau."
+        return 1
+    fi
 
-  # Redémarrer le service de gestionnaire de réseau
-  systemctl restart NetworkManager
-
-  # Vérifier si le redémarrage du service a réussi
-  if [ $? -eq 0 ]; then
-    echo "Le service de gestionnaire de réseau a été redémarré avec succès."
-  else
-  echo "Erreur lors du redémarrage du service de gestionnaire de réseau."
-    return 1
-  fi
-
-  return 0
+    return 0
 }
 
 reboot
